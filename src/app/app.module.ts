95204eeabd9da909dec2e516515e765f4e80fa71
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-02-19 18:09:24 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
